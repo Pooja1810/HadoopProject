408fcec98ebc9a2874da198f4a0343a5104b3d41
Pooja Sharma <Poojasharma.cse18@jecrc.ac.in>
2017-06-16 02:11:11 +0530

    hadoop project day 1 done

--- start.py
+++ start.py
@@ -11,6 +11,7 @@ time.sleep(2)
 user=raw_input("Enter user name to access project : ")
 password=getpass.getpass("Enetr user password : ")
 
+#To check user validity.
 if user == 'root' and password == 'redhat' :
 
 	print "\n\t\t\tACCESS GRANTED :-)"
