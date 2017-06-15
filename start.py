c972b8ed597036339868f9e8b2ebd4b9f33337f5
Pooja Sharma <Poojasharma.cse18@jecrc.ac.in>
2017-06-16 01:51:12 +0530

    Create start.py

--- start.py
+++ start.py
@@ -2,20 +2,18 @@
 
 import os,time,commands,sys,socket,getpass
 
-print "\t\t\t****************************************"
-print "\t\t\tCongrats you are in the world of data !!"
-print "\t\t\t****************************************"
+print "\t\t****************************************"
+print "\t\tCongrats you are in the world of data !!"
+print "\t\t****************************************"
 
 time.sleep(2)
 
 user=raw_input("Enter user name to access project : ")
 password=getpass.getpass("Enetr user password : ")
 
-print "-----------------------------------------------"
-
 if user == 'root' and password == 'redhat' :
 
-	print "\t\t\tACCESS GRANTED :-)"
+	print "\n\t\t\tACCESS GRANTED :-)"
 	execfile('menu.py')
 else :
 	print "wrong authentication !!"
